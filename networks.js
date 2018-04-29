15b4a7c2968911a8116fc9c19742b7f377ae4ab4
あ <>
2018-04-30 01:53:24 +0900

    Network

--- networks.js
+++ networks.js
@@ -6,48 +6,48 @@ var hex = function(hex) {
 
 exports.livenet = {
   name: 'livenet',
-  magic: hex('e5777746'),
-  addressVersion: 53,
-  privKeyVersion: 181,
-  P2SHVersion: 112,
+  magic: hex('70352205'),
+  addressVersion: 60,
+  privKeyVersion: 128+60,
+  P2SHVersion: 85,
   hkeyPublicVersion: 0x0,
   hkeyPrivateVersion: 0x0,
   genesisBlock: {
-    hash: hex('00000da3701e9b6eb1afa31d97ce6fc2c09cb7e165271b2e900a4275df8dd5bb'),
-    merkle_root: hex('0a8875e41a01a01dd0f1c759d5da353ee92561c089d19ef939510d6eda44c096'),
+    hash: hex('000004b7fdb8292c7a005e27ffc2e9916cc8e3891a922b671725fa7deda19783'),
+    merkle_root: hex('8e67a359fde8e5b6d03e4538a4461752bfe5bf4fd2885a5620eaea53d9b8929b'),
     height: 0,
     nonce: 0,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1511862256,
+    timestamp: 1400792000,
     bits: 0x1e0fffff,
   },
   dnsSeeds: [
     'dnsseed.neetcoin.jp'
   ],
-  defaultClientPort: 21011
+  defaultClientPort: 9393
 };
 
 exports.mainnet = exports.livenet;
 
 exports.testnet = {
   name: 'testnet',
-  magic: hex('82547825'),
+  magic: hex('cdf2c0ef'),
   addressVersion: 111,
   privKeyVersion: '239',
   P2SHVersion: 196,
   hkeyPublicVersion: 0x0,
   hkeyPrivateVersion: 0x0,
   genesisBlock: {
-    hash: hex('0000299b82787bca0132dbe67f8360d335ca114991993f67f48bdb462512e452'),
-    merkle_root: hex('0a8875e41a01a01dd0f1c759d5da353ee92561c089d19ef939510d6eda44c096'),
+    hash: hex('018e3cae72f38dcfe7effaabb7c643a427b88ef5a4ce120a0dec5315daa573af'),
+    merkle_root: hex('8e67a359fde8e5b6d03e4538a4461752bfe5bf4fd2885a5620eaea53d9b8929b'),
     height: 0,
     nonce: 0,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1511862256,
+    timestamp: 1400792000,
     bits: 0x1f00ffff,
   },
   dnsSeeds: [],
-  defaultClientPort: 21873
+  defaultClientPort: 19393
 };
